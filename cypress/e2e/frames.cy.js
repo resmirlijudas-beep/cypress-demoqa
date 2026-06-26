3080796f873c07ceafa30368ef590c25069bb558
resmirlijudas-beep <resmirlijudas-beep@users.noreply.github.com>
2026-06-26 02:20:44 +0000

    deploy: de61410872f332faa65e44e1625d41762f15d0fb

--- cypress/e2e/frames.cy.js
+++ cypress/e2e/frames.cy.js
@@ -10,7 +10,7 @@ describe('Frame hanling', () => {
 
 
     })
-    it('Verify first iframe test', () => {
+    it('Verify first iframe ', () => {
         
         Frames.getFrameBody(Frames.getFrame1())
         .find('#sampleHeading')
